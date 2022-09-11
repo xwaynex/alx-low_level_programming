3229960863fac07c9c925288bca433ff1044a608
xwaynex <waynexaint@gmail.com>
2022-09-11 02:37:42 +0100

    C variable

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - prints all possible combinations of two two-digit numbers
  * with only putchar()
  *
  * Return: 0 - Success
  */
-
 int main(void)
 {
 int i, o;
